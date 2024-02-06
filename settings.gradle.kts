98870967629fb3ab8d23f6af14dbd4c1a494c406
forestdesu <aa2652839@gmail.com>
2024-02-06 12:17:25 +0900

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "IvaKocha"
+include(":app")
+include(":app2")
+include(":second2")
+include(":HelloKitty")
+include(":dog")
